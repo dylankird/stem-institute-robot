934caf0eb783fcbdab33cc223636ef3f20c91368
Dylan Kirdahy <me@dylankirdahy.nyc>
2022-08-08 08:31:49 -0400

    Finished line follower template

--- line-follower-template/line-follower-template.ino
+++ line-follower-template/line-follower-template.ino
@@ -12,6 +12,18 @@
 //The speed the motors will drive at. Max speed is 255, 0 turns motors off:
 #define drive_speed 255
 
+/*
+TIPS:
+-We are using "analogWrite" instead of "digitalWrite" so that we can control the motor speed
+-Make sure your motor driver is plugged into PWM capable pins such as pins 3, 5, 6, and 9
+-In order to make the robot do a specific action in each case, copy and paste the four lines of code from the first if statement, and then modify them accordingly. You need to update all four motor driver pins each time you want to change how the robot moves.
+-If you leave the inside of an 'if' statement blank, the robot will continue moving the way it was before when it encounters that specific case
+-Try to adjust the drive_speed variable so that your robot drives at the right speed. Note that there is a certain point below which the motors will not turn on
+-Setting a pin to "drive_speed" makes it go at whatever speed you have specified in the above define statement, and setting it to 0 turns off the motor
+-You can manually set a speed (for example, 200) inside an analogWrite statement, or set some variant of drive_speed (such as drive_speed/2)
+*/
+
+
 //The variables that store whether or not each sensor detected the line:
 bool line_detected_left = 0;
 bool line_detected_middle = 0;
@@ -44,30 +56,34 @@ void loop() {
     analogWrite(right_forwards, drive_speed);
     analogWrite(right_backwards, 0);
   }
+
   else if(line_detected_left && !line_detected_middle && !line_detected_right) //If just the left sensor detects the line
   {
     //Code goes here
   }
+
   else if(!line_detected_left && !line_detected_middle && line_detected_right) //If just the right sensor detects the line
   {
     //Code goes here
   }
+
   else if(line_detected_left && line_detected_middle && !line_detected_right) //If the left sensor and the middle sensor detect the line
   {
     //Code goes here
   }
+
   else if(!line_detected_left && line_detected_middle && line_detected_right) //If the right sensor and the middle sensor detect the line
   {
     //Code goes here
   }
+
   else if(line_detected_left && line_detected_middle && line_detected_right) //If all three sensors detect the line
   {
     //Code goes here
   }
+
   else //If none of the sensors detect the line
   {
     //Code goes here
   }
- 
-  
 }
